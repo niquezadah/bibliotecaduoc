887dc68a41b5a7b86a8267820aad4bcdeb5f05fa
nicoGit <ni.quezadah@duocuc.cl>
2026-03-20 14:21:09 -0300

    Clase libro agregada

---
+++ src/main/java/com/example/bibliotecaduoc/model/Libro.java
@@ -0,0 +1,11 @@
+package com.example.bibliotecaduoc.model;
+
+public class Libro {
+    
+    private int id;
+    private String isbn;
+    private String titulo;
+    private String editorial;
+    private String fechaPublicacion;
+    private String autor;
+}
