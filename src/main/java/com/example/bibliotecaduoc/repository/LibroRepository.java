c3e16984927110a740f1aac05e6fb2d15923835c
nicoGit <ni.quezadah@duocuc.cl>
2026-03-25 12:39:53 -0300

    se agregaron import de ArrayList, list y el import de la clase Libro, tambien el Array para guardar los libros y el metodo que retorna los libros

--- src/main/java/com/example/bibliotecaduoc/repository/LibroRepository.java
+++ src/main/java/com/example/bibliotecaduoc/repository/LibroRepository.java
@@ -1,10 +1,20 @@
 package com.example.bibliotecaduoc.repository;
 
+import com.example.bibliotecaduoc.model.Libro;
 import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Repository
 public class LibroRepository {
 
+    // arreglo que guardara los libros
     private List<Libro> listaLibros = new ArrayList<>();
+
+    // metodo que retorna los libros
+    public List<Libro> obteneLibros(){
+        return listaLibros;
+    }
     
 }
