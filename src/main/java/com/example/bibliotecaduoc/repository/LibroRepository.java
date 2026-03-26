3c6f2bec159340d576cd04333d8fd3b75939298a
nicoGit <ni.quezadah@duocuc.cl>
2026-03-26 13:15:46 -0300

    se anadio metodo para buscar libros por isbn

--- src/main/java/com/example/bibliotecaduoc/repository/LibroRepository.java
+++ src/main/java/com/example/bibliotecaduoc/repository/LibroRepository.java
@@ -17,4 +17,33 @@ public class LibroRepository {
         return listaLibros;
     }
     
-}
+    //metodo que busca un libro por id
+    public Libro buscarPorId(int id) {
+        for (Libro libro : listaLibros) {
+            if (libro.getId() == id) {
+                return libro;
+            }
+        }
+        return null;
+    }
+
+    //metodo que busca un libro por isbn
+    public Libro buscarPorIsbn(String isbn) {
+        for (Libro libro : listaLibros) {
+            if (libro.getIsbn().equals(isbn)){
+                return libro;
+            }
+        }
+        return null;
+    }
+
+
+
+
+
+
+
+
+
+
+}   
