bb807df2a8fb99ad7542fe7280e624076460f826
nicoGit <ni.quezadah@duocuc.cl>
2026-03-28 12:21:50 -0300

    se anadieron las anotaciones '@RestController', '@Autowired' y los imports correspondientes a la nueva clase tambien recientemente creada

---
+++ src/main/java/com/example/bibliotecaduoc/controller/LibroController.java
@@ -0,0 +1,19 @@
+package com.example.bibliotecaduoc.controller;
+
+import com.example.bibliotecaduoc.model.Libro;
+import com.example.bibliotecaduoc.service.LibroService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/libros")
+public class LibroController {
+    
+    @Autowired
+    private LibroService libroService;
+
+
+
+}
