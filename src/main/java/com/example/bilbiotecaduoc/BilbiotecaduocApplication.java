79a15155123e1e7a0e4984c5d5d322458ee59da0
nicoGit <ni.quezadah@duocuc.cl>
2026-04-01 00:53:51 -0300

    se creo 'bibliotecaduoc + main'

---
+++ src/main/java/com/example/bilbiotecaduoc/BilbiotecaduocApplication.java
@@ -0,0 +1,13 @@
+package com.example.bilbiotecaduoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BilbiotecaduocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BilbiotecaduocApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/bilbiotecaduoc/BilbiotecaduocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bilbiotecaduoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BilbiotecaduocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
